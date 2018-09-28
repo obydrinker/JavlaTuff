21b56e8cd905e8aaf7ac0e440c143a9df43be653
Noel Mortensen <noel.mortensen@elev.ga.lbs.se>
2018-09-28 15:06:07 +0200

    något fint

---
+++ Assets/conditions.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conditions : MonoBehaviour {
+    public bool ifBool;
+    public int value;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (ifBool == true)
+        {
+            print("ja");
+        }
+        else
+        {
+            print("nej");
+        }
+
+        
+    }
+    // Update is called once per frame
+
+    // != inte lika med 
+    // == lika med
+    // < mindre än
+    // > större än
+    // <= mindre eller lika med 
+    // >= större eller lika med
+    // && och (shift 6)
+    // || eller (altgr <)
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            print("Javla Tuff");
+           value = Random.Range(0, 10);
+        }
+
+        //if (value != 10)
+        //{
+        //    print("correct");
+        //}
+        //else
+        //{
+        //    print("incorrect");
+        //}
+    }
+}
+   
+
+
+   
+        
---
+++ Assets/testAssignment.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testAssignment : MonoBehaviour
+{
+
+    public int userValue;
+    int dice = 10;
+    public int playerHP;
+    public int dragonHP;
+    bool start = false;
+    int dragonHitChance;
+    int ultimateDragonKnightSlayer;
+    int playerMinDamage;
+    int playerMaxDamage;
+
+
+
+
+
+
+
+
+    void Uppgift1()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            userValue += 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            userValue /= 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print(userValue);
+        }
+    }
+
+    void Uppgift2()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            dice += (Random.Range(1, 7)) - (Random.Range(1, 7));
+            print(dice);
+        }
+    }
+    void Uppgift3()
+    {
+        if (start == false)
+        {
+            
+            playerHP = 100;
+            dragonHP = (Random.Range(100, 151));
+            ultimateDragonKnightSlayer = Random.Range(1, 11);
+            if (ultimateDragonKnightSlayer == 1)
+
+            {
+                dragonHP = dragonHP * 2;
+            }
+            start = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dragonHitChance = (Random.Range(0, 2));
+            if (dragonHitChance >= 1)
+            {
+                playerHP -= (Random.Range(10, 21));
+
+
+            }
+           // fkn ta tag i!!! dragonHP -= (Random.Range(Random.Range(3, 5)));
+        }
+        if (dragonHP <= 0)
+        {
+            print("player wins");
+            start = false;
+        }
+        if (playerHP <= 0)
+        {
+            print("you died");
+            start = false;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Uppgift1();
+        Uppgift2();
+        Uppgift3();
+    }
+
+}
